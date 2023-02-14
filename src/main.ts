b36907014f28b1888dab50ce09feb8c51614983d
Matthieu Chollet <matthieuchollet@MacBook-Pro-de-Matthieu.local>
2023-02-14 15:21:27 +0100

    docs: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Todo example')
+    .setDescription('The Todo Api description')
+    .setVersion('1.0')
+    .addTag('todos')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
